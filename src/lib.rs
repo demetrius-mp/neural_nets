1a5ec242d716a2d548876f1472f53aa957180378
Demetrius Panovitch <demetrius.mp789@gmail.com>
2022-09-16 11:37:16 -0400

    feat: add dot product and sum

---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+/// Equivalent to running the dot product between `a` and `b`,
+/// then summing every entry of the result.
+/// - If one of the entries is empty, it returns 0.0.
+/// - If the length of the entries is different, it goes up to the smallest one.
+///
+/// # Arguments
+///
+/// * `a` - A `Vector<f64>` containing 0 or more items.
+/// * `b` - A `Vector<f64>` containing 0 or more items.
+///
+/// # Examples
+///
+/// ```
+/// use numerical::dot_product_and_sum;
+///
+/// let a: Vec<f64> = vec![1.0, 2.0];
+/// let b: Vec<f64> = vec![3.0, 4.0];
+/// let result = dot_product_and_sum(&a, &b);
+/// assert_eq!(result, 11.0);
+/// ```
+pub fn dot_product_and_sum(a: &Vec<f64>, b: &Vec<f64>) -> f64 {
+    a.iter().zip(b).fold(0.0, |acc, (a_element, b_element)| {
+        acc + (a_element * b_element)
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn dot_product_and_sum_equal_lengths() {
+        let result = dot_product_and_sum(&vec![1.0, 2.0], &vec![1.0, 2.0]);
+        assert_eq!(result, 5.0);
+    }
+
+    #[test]
+    fn dot_product_and_sum_a_bigger_than_b() {
+        let result = dot_product_and_sum(&vec![1.0, 2.0, 3.0], &vec![1.0, 2.0]);
+        assert_eq!(result, 5.0);
+    }
+
+    #[test]
+    fn dot_product_and_sum_a_is_empty() {
+        let result = dot_product_and_sum(&vec![], &vec![1.0, 2.0, 3.0]);
+        assert_eq!(result, 0.0);
+    }
+
+    #[test]
+    fn dot_product_and_sum_b_is_empty() {
+        let result = dot_product_and_sum(&vec![1.0, 2.0, 3.0], &vec![]);
+        assert_eq!(result, 0.0);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use numerical::dot_product_and_sum;
+
 fn main() {
-    println!("Hello, world!");
+    let result = dot_product_and_sum(&vec![1.0, 2.0], &vec![1.0, 2.0]);
+
+    println!("{result}");
 }
