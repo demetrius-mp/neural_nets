60d999e78e8ea69958adf837bfc80129bf1afc54
Demetrius Panovitch <demetrius.mp789@gmail.com>
2022-09-17 17:04:50 -0400

    feat: use nalgebra for linear algebra computation

--- src/lib.rs
+++ src/lib.rs
@@ -1,75 +1,179 @@
-/// Equivalent to running the dot product between `a` and `b`,
-/// then summing every entry of the result.
-/// - If one of the entries is empty, it returns 0.0.
-/// - If the length of the entries is different, it goes up to the smallest one.
-///
+use nalgebra::{DMatrix, Dynamic, Matrix, VecStorage};
+
+/// Runs a convolution on the given channels, using the given kernel.
 /// # Arguments
 ///
-/// * `a` - A `Vector<f64>` containing 0 or more items.
-/// * `b` - A `Vector<f64>` containing 0 or more items.
+/// * `kernel` - A vector of matrices, where each matrix will be convolved with a channel of the image.
+/// * `channels` - A vector of matrices, where each matrix represents a channel of the image.
 ///
 /// # Examples
 ///
 /// ```
-/// use numerical::dot_product_and_sum;
+/// use numerical::convolve;
+/// use nalgebra::{DMatrix};
 ///
-/// let a: Vec<f64> = vec![1.0, 2.0];
-/// let b: Vec<f64> = vec![3.0, 4.0];
-/// let result = dot_product_and_sum(&a, &b);
-/// assert_eq!(result, 11.0);
+/// #[rustfmt::skip]
+/// let kernel = DMatrix::from_vec(3, 3, vec![
+///     0.0, 1.0, 2.0,
+///     3.0, 4.0, 5.0,
+///     6.0, 7.0, 8.0,
+/// ]).transpose();
+///
+/// #[rustfmt::skip]
+/// let channel = DMatrix::from_vec(
+///     4,
+///     4,
+///     vec![
+///         0.0, 1.0, 0.0, 0.0,
+///         1.0, 1.0, 1.0, 1.0,
+///         0.0, 1.0, 1.0, 1.0,
+///         1.0, 0.0, 0.0, 1.0,
+///     ],
+/// ).transpose();
+///
+/// let result = convolve(
+///     &vec![kernel],
+///     &vec![channel],
+/// );
+///
+/// #[rustfmt::skip]
+/// let expected = DMatrix::from_vec(2, 2, vec![
+///     28.0, 33.0,
+///     18.0, 23.0
+/// ]).transpose();
+///
+/// assert_eq!(result, expected);
 /// ```
-pub fn dot_product_and_sum(a: &Vec<f64>, b: &Vec<f64>) -> f64 {
-    a.iter().zip(b).fold(0.0, |acc, (a_element, b_element)| {
-        acc + (a_element * b_element)
-    })
+pub fn convolve(kernel: &Vec<Container>, channels: &Vec<Container>) -> Container {
+    let nrows = channels[0].nrows() - kernel[0].nrows() + 1;
+    let ncols = channels[0].ncols() - kernel[0].ncols() + 1;
+
+    let mut convoluted = DMatrix::from_vec(nrows, ncols, vec![0.0; nrows * ncols]);
+
+    for i in 0..nrows {
+        for j in 0..ncols {
+            for (kernel_channel, channel) in kernel.iter().zip(channels) {
+                let channel_slice =
+                    channel.slice((i, j), (kernel_channel.nrows(), kernel_channel.ncols()));
+                let product = kernel_channel.component_mul(&channel_slice);
+
+                convoluted[(i, j)] += product.sum();
+            }
+        }
+    }
+
+    convoluted
 }
+
+type Container = Matrix<f64, Dynamic, Dynamic, VecStorage<f64, Dynamic, Dynamic>>;
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use nalgebra::DMatrix;
 
     #[test]
-    fn dot_product_and_sum_equal_lengths() {
-        let a = vec![1.0, 2.0];
-        let b = vec![1.0, 2.0];
-        let result = dot_product_and_sum(&a, &b);
+    fn convolve_single_channel() {
+        #[rustfmt::skip]
+        let kernel = DMatrix::from_vec(3, 3, vec![
+            0.0, 1.0, 2.0,
+            3.0, 4.0, 5.0,
+            6.0, 7.0, 8.0,
+        ]).transpose();
 
-        assert_eq!(result, 5.0);
+        #[rustfmt::skip]
+        let channel = DMatrix::from_vec(
+            4,
+            4,
+            vec![
+                0.0, 1.0, 0.0, 0.0,
+                1.0, 1.0, 1.0, 1.0,
+                0.0, 1.0, 1.0, 1.0,
+                1.0, 0.0, 0.0, 1.0,
+            ],
+        ).transpose();
+
+        let result = convolve(&vec![kernel], &vec![channel]);
+
+        #[rustfmt::skip]
+        let expected = DMatrix::from_vec(2, 2, vec![
+            28.0, 33.0,
+            18.0, 23.0
+        ]).transpose();
+
+        assert_eq!(result, expected);
     }
 
     #[test]
-    fn dot_product_and_sum_a_bigger_than_b() {
-        let a = vec![1.0, 2.0, 3.0];
-        let b = vec![1.0, 2.0];
-        let result = dot_product_and_sum(&a, &b);
+    fn convolve_multi_channel() {
+        #[rustfmt::skip]
+        let kernel = DMatrix::from_vec(3, 3, vec![
+            0.0, 1.0, 2.0,
+            3.0, 4.0, 5.0,
+            6.0, 7.0, 8.0,
+        ]).transpose();
 
-        assert_eq!(result, 5.0);
-    }
+        #[rustfmt::skip]
+        let kernel2 = DMatrix::from_vec(3, 3, vec![
+            9.0, 10.0, 11.0,
+            12.0, 13.0, 14.0,
+            15.0, 16.0, 17.0
+        ]).transpose();
 
-    #[test]
-    fn dot_product_and_sum_b_bigger_than_a() {
-        let a = vec![1.0, 2.0];
-        let b = vec![1.0, 2.0, 3.0];
-        let result = dot_product_and_sum(&a, &b);
+        #[rustfmt::skip]
+        let kernel3 = DMatrix::from_vec(3, 3, vec![
+            18.0, 19.0, 20.0,
+            21.0, 22.0, 23.0,
+            24.0, 25.0, 26.0
+        ]).transpose();
 
-        assert_eq!(result, 5.0);
-    }
+        #[rustfmt::skip]
+        let channel = DMatrix::from_vec(
+            4,
+            4,
+            vec![
+                0.0, 1.0, 0.0, 0.0,
+                1.0, 1.0, 1.0, 1.0,
+                0.0, 1.0, 1.0, 1.0,
+                1.0, 0.0, 0.0, 1.0,
+            ],
+        ).transpose();
 
-    #[test]
-    fn dot_product_and_sum_a_is_empty_b_is_filled() {
-        let a: Vec<f64> = vec![];
-        let b = vec![1.0, 2.0];
-        let result = dot_product_and_sum(&a, &b);
+        #[rustfmt::skip]
+        let channel2 = DMatrix::from_vec(
+            4,
+            4,
+            vec![
+                0.0, 0.0, 0.0, 1.0,
+                0.0, 1.0, 0.0, 1.0,
+                1.0, 0.0, 0.0, 0.0,
+                1.0, 1.0, 1.0, 1.0,
+            ],
+        ).transpose();
 
-        assert_eq!(result, 0.0);
-    }
+        #[rustfmt::skip]
+        let channel3 = DMatrix::from_vec(
+            4,
+            4,
+            vec![
+                1.0, 1.0, 0.0, 0.0,
+                0.0, 1.0, 1.0, 0.0,
+                0.0, 1.0, 1.0, 1.0,
+                1.0, 0.0, 1.0, 0.0,
+            ],
+        ).transpose();
 
-    #[test]
-    fn dot_product_and_sum_b_is_empty_a_is_filled() {
-        let a = vec![1.0, 2.0];
-        let b: Vec<f64> = vec![];
-        let result = dot_product_and_sum(&a, &b);
+        let result = convolve(
+            &vec![kernel, kernel2, kernel3],
+            &vec![channel, channel2, channel3],
+        );
 
-        assert_eq!(result, 0.0);
+        #[rustfmt::skip]
+        let expected = DMatrix::from_vec(2, 2, vec![
+            189.0, 206.0,
+            222.0, 219.0
+        ]).transpose();
+
+        assert_eq!(result, expected);
     }
 }
