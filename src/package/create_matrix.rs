b9d41cc3a4948bca06b0ef202f36a43315d8f2bd
Demetrius Panovitch <demetrius.mp789@gmail.com>
2022-09-21 17:03:09 -0400

    docs: create_matrix documentation

--- src/package/create_matrix.rs
+++ src/package/create_matrix.rs
@@ -2,6 +2,35 @@ use nalgebra::{DMatrix, Dynamic, Matrix as NMatrix, VecStorage};
 
 pub type Matrix = NMatrix<f64, Dynamic, Dynamic, VecStorage<f64, Dynamic, Dynamic>>;
 
+/// Creates a matrix. It is used as entry for many functions of this package.
+///
+/// This is just a wrapper for running  `DMatrix::from_row_iterator(nrows, ncols, data)`
+///
+/// # Arguments
+///
+/// * `nrows` - Number of rows of the matrix.
+/// * `ncols` - Number of columns of the matrix.
+/// * `data` - Vec<f64> containing the entries of the matrix.
+///
+/// # Examples
+///
+/// ```
+/// use numerical::create_matrix;
+///
+/// let nrows = 3;
+/// let ncols = 3;
+///
+/// #[rustfmt::skip]
+/// let data = vec![
+///     1.0, 2.0, 3.0,
+///     4.0, 5.0, 6.0,
+///     7.0, 8.0, 9.0,
+/// ];
+///
+/// let m = create_matrix(nrows, ncols, data);
+///
+/// assert_eq!(m[(1, 1)], 5.0);
+/// ```
 pub fn create_matrix(nrows: usize, ncols: usize, data: Vec<f64>) -> Matrix {
     DMatrix::from_row_iterator(nrows, ncols, data)
 }
