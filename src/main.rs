1c84dbca4d5a6c207b4db6661dce5710a4cdbcbe
Demetrius Panovitch <demetrius.mp789@gmail.com>
2022-09-22 21:50:24 -0400

    feat: add batch logistic regression

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use numerical::{
     create_matrix,
-    stochastic_logistic_regression,
+    batch_logistic_regression,
+    stochastic_logistic_regression
 };
 
 fn main() {
@@ -35,6 +36,9 @@ fn main() {
             1.0, 1.0, 1.0
         ]
     );
+
+    let res = batch_logistic_regression(&x, &y, &initial_theta, 0.001, 1);
+    println!("{}", res);
 
     let res = stochastic_logistic_regression(&x, &y, &initial_theta, 0.001, 10000);
     println!("{}", res);
--- src/package/logistic_regression.rs
+++ src/package/logistic_regression.rs
@@ -4,6 +4,35 @@ fn sigmoid(x: f64) -> f64 {
     1.0 / (1.0 + (-x).exp())
 }
 
+pub fn batch_logistic_regression(
+    x: &Matrix,
+    y: &Matrix,
+    initial_theta: &Matrix,
+    alpha: f64,
+    epochs: u128,
+) -> Matrix {
+    let mut theta = initial_theta.clone();
+
+    let x_transposed = x.transpose();
+    let y_transposed = y.transpose();
+    let factor = 1.0 / x.nrows() as f64;
+
+    for _ in 0..epochs {
+        let theta_x_t = &theta * &x_transposed;
+        let gx = &theta_x_t.map(sigmoid);
+        let gx_minus_y = gx - &y_transposed;
+        let one_minus_gx = &gx.map(|v| 1.0 - v);
+
+        let mut delta = gx_minus_y.component_mul(gx).component_mul(one_minus_gx);
+        delta = delta * x;
+        delta = delta.scale(factor);
+
+        theta = theta - alpha * delta;
+    }
+
+    theta
+}
+
 pub fn stochastic_logistic_regression(
     x: &Matrix,
     y: &Matrix,
