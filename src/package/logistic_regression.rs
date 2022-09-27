5abe92768d1eab6fd8bebda3c4473fe4c235910e
Demetrius Panovitch <demetrius.mp789@gmail.com>
2022-09-27 19:20:17 -0400

    refactor: move logistic regression to struct

--- src/package/logistic_regression.rs
+++ src/package/logistic_regression.rs
@@ -4,43 +4,56 @@ fn sigmoid(x: f64) -> f64 {
     1.0 / (1.0 + (-x).exp())
 }
 
-pub fn mini_batch_logistic_regression(
-    x: &Matrix,
-    y: &Matrix,
-    initial_theta: &Matrix,
+pub struct LogisticRegression<'a> {
+    x: &'a Matrix,
+    y: &'a Matrix,
+    initial_theta: &'a Matrix,
     alpha: f64,
     epochs: u128,
-    mini_batch_size: usize,
-) -> Matrix {
-    let mut theta = initial_theta.clone();
-
-    for _ in 0..epochs {
-        for i in (0..x.nrows()).step_by(mini_batch_size) {
-            let x_mini_batch = x.rows(i, mini_batch_size);
-            let y_mini_batch = y.rows(i, mini_batch_size);
-
-            let factor = 1.0 / mini_batch_size as f64;
-
-            let theta_x_t = &theta * &x_mini_batch.transpose();
-            let gx = &theta_x_t.map(sigmoid);
-            let gx_minus_y = gx - &y_mini_batch.transpose();
-            let one_minus_gx = &gx.map(|v| 1.0 - v);
-
-            let mut delta = gx_minus_y.component_mul(gx).component_mul(one_minus_gx);
-            delta = delta * x_mini_batch;
-            delta = delta.scale(factor);
-
-            theta -= alpha * delta;
-        }
-    }
-
-    theta
 }
 
-pub fn predict(theta: &Matrix, x: &Matrix) -> bool {
-    let res = sigmoid(theta.component_mul(x).sum());
+impl<'a> LogisticRegression<'a> {
+    pub fn new(
+        x: &'a Matrix,
+        y: &'a Matrix,
+        initial_theta: &'a Matrix,
+        alpha: f64,
+        epochs: u128,
+    ) -> Self {
+        Self { x, y, initial_theta, alpha, epochs }
+    }
 
-    res > 0.5
+    pub fn fit(&self, mini_batch_size: usize) -> Matrix {
+        let mut theta = self.initial_theta.clone();
+
+        for _ in 0..self.epochs {
+            for i in (0..self.x.nrows()).step_by(mini_batch_size) {
+                let x_mini_batch = self.x.rows(i, mini_batch_size);
+                let y_mini_batch = self.y.rows(i, mini_batch_size);
+
+                let factor = 1.0 / mini_batch_size as f64;
+
+                let theta_x_t = &theta * &x_mini_batch.transpose();
+                let gx = &theta_x_t.map(sigmoid);
+                let gx_minus_y = gx - &y_mini_batch.transpose();
+                let one_minus_gx = &gx.map(|v| 1.0 - v);
+
+                let mut delta = gx_minus_y.component_mul(gx).component_mul(one_minus_gx);
+                delta = delta * x_mini_batch;
+                delta = delta.scale(factor);
+
+                theta -= self.alpha * delta;
+            }
+        }
+
+        theta
+    }
+
+    pub fn predict(theta: &Matrix, x: &Matrix) -> bool {
+        let res = sigmoid(theta.component_mul(x).sum());
+    
+        res > 0.5
+    }
 }
 
 #[cfg(test)]
@@ -128,10 +141,11 @@ mod tests {
     #[test]
     fn mini_batch_logistic_regression_on_logical_and_problem() {
         let data = LogicalAndProblemData::new();
-        let theta = mini_batch_logistic_regression(&data.x, &data.y, &data.initial_theta, 0.5, 1000, 1);
+        let lr = LogisticRegression::new(&data.x, &data.y, &data.initial_theta, 0.5, 1000);
+        let theta = lr.fit(1);
 
         for prediction in data.predictions {
-            let result = predict(&theta, &prediction.input);
+            let result = LogisticRegression::predict(&theta, &prediction.input);
             assert_eq!(result, prediction.output);
         }
     }
